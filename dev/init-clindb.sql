d744e4df674c898ea997633092f8143efec3fdf0
Ivan Cvitkovic <ivanc@uw.edu>
2024-10-08 10:38:30 -0700

    Add RADAR site

--- dev/init-clindb.sql
+++ dev/init-clindb.sql
@@ -1,1 +1,3 @@
 -- MariaDB initialization script; run on first start only
+CREATE DATABASE IF NOT EXISTS radar;
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON radar TO 'db-user'@'localhost';
