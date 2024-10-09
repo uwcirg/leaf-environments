d44e730176a62f1186e21a9599146dc85b8e7fbd
Ivan Cvitkovic <ivanc@uw.edu>
2024-10-09 06:24:51 -0700

    Restrict permissions on leaf app DB user

--- dev/init-clindb.sql
+++ dev/init-clindb.sql
@@ -1,3 +1,3 @@
 -- MariaDB initialization script; run on first start only
 CREATE DATABASE IF NOT EXISTS radar;
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON radar TO 'db-user'@'localhost';
+GRANT SELECT, CREATE TEMPORARY TABLES ON radar.* TO `db-user`@`%`;
