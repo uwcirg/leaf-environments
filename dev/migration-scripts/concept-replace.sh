d851a3a5569ede80b182e9c1f3a6c57133ab9bef
Ryan Patrick Kyle <kyle@uw.edu>
2024-10-24 21:39:43 -0700

    add comments

--- dev/migration-scripts/concept-replace.sh
+++ dev/migration-scripts/concept-replace.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Throw an error if no argument is passed -- the correct syntax is
+#   concept-replace.sh <docker-service-name>
+
 if [ "$#" -eq 0 ]; then
     echo "Error: No arguments provided. You must specify a single docker service for concept replacement."
     exit 1
@@ -14,6 +17,8 @@ container_name=$(docker inspect --format '{{.Name}}' $(docker compose ps -q $1) 
 echo "Copying SQL inserts to $container_name ..."
 echo " "
 
+# Copy the insert statements into the Docker container
+
 docker cp appConcept.sql "$container_name:/tmp/appConcept.sql"
 docker cp appConceptSqlSet.sql "$container_name:/tmp/appConceptSqlSet.sql"
 docker cp appSpecialization.sql "$container_name:/tmp/appSpecialization.sql"
@@ -22,6 +27,8 @@ docker cp appSpecializationGroup.sql "$container_name:/tmp/appSpecializationGrou
 echo "Copying completed."
 echo " "
 
-echo "Deleting old concepts ..."
+echo "Deleting old concepts and inserting new ones ..."
+
+# Insert the new concepts and specializations
 
 cat insert_concepts.sh | docker exec -i "$container_name" bash
--- dev/migration-scripts/generate_inserts.sh
+++ dev/migration-scripts/generate_inserts.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# output insert statements for required concept-related tables
 /opt/mssql-tools/bin/sqlcmd -d LeafDB -U sa -P ${SA_PASSWORD} -Q "EXEC sp_generate_inserts 'Specialization', @owner = 'app'" -o /tmp/appSpecialization.sql -y 0
 /opt/mssql-tools/bin/sqlcmd -d LeafDB -U sa -P ${SA_PASSWORD} -Q "EXEC sp_generate_inserts 'Concept', @owner = 'app'" -o /tmp/appConcept.sql -y 0
 /opt/mssql-tools/bin/sqlcmd -d LeafDB -U sa -P ${SA_PASSWORD} -Q "EXEC sp_generate_inserts 'ConceptSqlSet', @owner = 'app'" -o /tmp/appConceptSqlSet.sql -y 0
 /opt/mssql-tools/bin/sqlcmd -d LeafDB -U sa -P ${SA_PASSWORD} -Q "EXEC sp_generate_inserts 'SpecializationGroup', @owner = 'app'" -o /tmp/appSpecializationGroup.sql -y 0
 
+# remove trailing spaces within quoted strings, as these were not present in the original table
 sed -Ei "s/('[^']*[^[:space:]])[[:space:]]*'/\1'/g" /tmp/appConcept.sql
 sed -Ei "s/('[^']*[^[:space:]])[[:space:]]*'/\1'/g" /tmp/appSpecialization.sql
